52872a17f0d587734a424b69a010985f381b74c7
Nick Dawe <nick@towananalytics.com>
2021-09-07 00:15:41 +0800

    updates

--- src/save to excel.R
+++ src/save to excel.R
@@ -4,9 +4,12 @@ library(openxlsx)
 
 wb <- openxlsx::createWorkbook()
 
+sheet1.name <- "Tasks"
+sheet2.name <- "Issues"
+
 # Insert Tabs
-addWorksheet(wb, "Summary", gridLines = FALSE)
-addWorksheet(wb, "Issues", gridLines = FALSE)
+addWorksheet(wb, sheet1.name, gridLines = FALSE)
+addWorksheet(wb, sheet2.name, gridLines = FALSE)
 
 issues <- open_issues %>% 
   select(Task.Name, Description, Assigned.To) %>% 
@@ -14,14 +17,14 @@ issues <- open_issues %>%
          "Assigned To" = Assigned.To)
 
 # Populate with data
-openxlsx::writeData(wb = wb, x = wk_report, startCol = 4, startRow = 4, borders = "rows", sheet = "Summary")
-openxlsx::writeData(wb = wb, x = issues, startCol = 1, startRow = 4, borders = "rows", sheet = "Issues")
+openxlsx::writeData(wb = wb, x = wk_report, startCol = 4, startRow = 4, borders = "rows", sheet = sheet1.name)
+openxlsx::writeData(wb = wb, x = issues, startCol = 1, startRow = 4, borders = "rows", sheet = sheet2.name)
 
 # Format layouts and styles
 
-setColWidths(wb, "Summary", cols = c(4, 5, 6, 7, 8, 9, 10, 11, 12), widths = c(10, 13, 13, 13, 13, 13, 13, 13, 13))
+setColWidths(wb, sheet1.name, cols = c(4, 5, 6, 7, 8, 9, 10, 11, 12), widths = c(10, 13, 13, 13, 13, 13, 13, 13, 13))
 
-setColWidths(wb, "Issues", cols = c(1, 2, 3), widths = c(30, 55, 18))
+setColWidths(wb, sheet2.name, cols = c(1, 2, 3), widths = c(30, 55, 18))
 
 ## create and add a style to the column headers
 headerStyle <- openxlsx::createStyle(
@@ -44,38 +47,46 @@ underline.header <- openxlsx::createStyle( # Create a green bar at the top of th
   fgFill = "#FFFFFF", border = "Top", borderColour = ppa_cols[2]
 )
 
-openxlsx::addStyle(wb, sheet = "Summary", style = bodyStyle, rows = 4, cols = c(4:11), gridExpand = TRUE)
-openxlsx::addStyle(wb, sheet = "Summary", style = headerStyle, rows = 4, cols = c(4:11), gridExpand = TRUE)
-openxlsx::addStyle(wb, sheet = "Summary", style = highlight.row, rows = 6, cols = c(4:11), gridExpand = TRUE)
-openxlsx::addStyle(wb, sheet = "Summary", style = underline.header, rows = 1, cols = c(1:15), gridExpand = TRUE)
+percentage.style <- openxlsx::createStyle(numFmt = "PERCENTAGE"
+                                          )
 
-openxlsx::addStyle(wb, sheet = "Issues", style = bodyStyle, rows = c(5:(4 + nrow(issues))), cols = c(1:3), gridExpand = TRUE)
-openxlsx::addStyle(wb, sheet = "Issues", style = underline.header, rows = 1, cols = c(1:3), gridExpand = TRUE)
-openxlsx::addStyle(wb, sheet = "Issues", style = headerStyle, rows = 4, cols = c(1:3), gridExpand = TRUE)
+
+openxlsx::addStyle(wb, sheet = sheet1.name, style = bodyStyle, rows = 4, cols = c(4:12), gridExpand = TRUE)
+openxlsx::addStyle(wb, sheet = sheet1.name, style = headerStyle, rows = 4, cols = c(4:12), gridExpand = TRUE)
+openxlsx::addStyle(wb, sheet = sheet1.name, style = highlight.row, rows = 7, cols = c(4:12), gridExpand = TRUE)
+openxlsx::addStyle(wb, sheet = sheet1.name, style = underline.header, rows = 1, cols = c(1:15), gridExpand = TRUE)
+openxlsx::addStyle(wb, sheet = sheet1.name, style = createStyle(numFmt = "##0%"), rows = c(5:9), cols = c(12), gridExpand = TRUE, stack = TRUE)
+
+openxlsx::addStyle(wb, sheet = sheet2.name, style = bodyStyle, rows = c(5:(4 + nrow(issues))), cols = c(1:3), gridExpand = TRUE)
+openxlsx::addStyle(wb, sheet = sheet2.name, style = underline.header, rows = 1, cols = c(1:3), gridExpand = TRUE)
+openxlsx::addStyle(wb, sheet = sheet2.name, style = headerStyle, rows = 4, cols = c(1:3), gridExpand = TRUE)
 
 
 print(tasks_over_time)
-insertPlot(wb, sheet = 1, xy = c("C", 11), width = 15, height = 10, fileType = "png", units = "cm")
+insertPlot(wb, sheet = 1, xy = c("B", 11), width = 15, height = 10, fileType = "png", units = "cm")
 
 print(late_versus_ontime)
 insertPlot(wb, sheet = 1, xy = c("I", 11), width = 15, height = 10, fileType = "png", units = "cm")
 
+print(completion_ratio_plt)
+insertPlot(wb, sheet = 1, xy = c("B", 31), width = 15, height = 9, fileType = "png", units = "cm")
+
 print(plot_tasks_assigned_to)
-insertPlot(wb, sheet = 1, xy = c("F", 31), width = 15, height = 9, fileType = "png", units = "cm")
+insertPlot(wb, sheet = 1, xy = c("I", 31), width = 15, height = 9, fileType = "png", units = "cm")
 
 # Format Summary page layout
-openxlsx::pageSetup(wb, sheet = "Summary", orientation = "landscape", paperSize = 8)
-openxlsx::pageSetup(wb, sheet = "Issues", orientation = "portrait", fitToWidth = TRUE)
+openxlsx::pageSetup(wb, sheet = sheet1.name, orientation = "landscape", paperSize = 8)
+openxlsx::pageSetup(wb, sheet = sheet2.name, orientation = "portrait", fitToWidth = TRUE)
 
 # setHeader(wb, "TASK REPORT", position = "left")
-setHeaderFooter(wb, sheet = "Summary", 
-                header = c('&"Arial"&B&14&K008C98TASK REPORT', 
+setHeaderFooter(wb, sheet = sheet1.name, 
+                header = c(paste0('&"Arial"&B&14&K008C98TASK REPORT - ', plan_name_to_filter), 
                                                   NA, 
                                                   NA),
                 footer = c("Printed On: &[Date]", NA, "Page &[Page] of &[Pages]"),)
 
-setHeaderFooter(wb, sheet = "Issues", 
-                header = c('&"Arial"&B&14&K008C98ISSUES REGISTER', 
+setHeaderFooter(wb, sheet = sheet2.name, 
+                header = c(paste0('&"Arial"&B&14&K008C98CURRENT ISSUES - ', plan_name_to_filter), 
                            NA, 
                            NA),
                 footer = c("Printed On: &[Date]", NA, "Page &[Page] of &[Pages]"),)
